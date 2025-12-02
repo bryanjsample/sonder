89292243bc72b5e64adc8f81fe762f7f65e51d9b
Bryan Sample <bryanjsample@gmail.com>
2025-12-02 12:24:47 -0700

    Initial commit

---
+++ sonder/sonderApp.swift
@@ -0,0 +1,17 @@
+//
+//  sonderApp.swift
+//  sonder
+//
+//  Created by Bryan Sample on 11/23/25.
+//
+
+import SwiftUI
+
+@main
+struct sonderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sonderTests/sonderTests.swift
@@ -0,0 +1,17 @@
+//
+//  sonderTests.swift
+//  sonderTests
+//
+//  Created by Bryan Sample on 11/23/25.
+//
+
+import Testing
+@testable import sonder
+
+struct sonderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
